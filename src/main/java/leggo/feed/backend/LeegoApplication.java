c84d4ef6d50c30ca85ef64a26a9eb27c84476aa0
KorYejinLee <koryjlee@gmail.com>
2024-11-26 20:56:17 +0900

    first commit

---
+++ src/main/java/leggo/feed/backend/LeegoApplication.java
@@ -0,0 +1,13 @@
+package leggo.feed.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeegoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeegoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/leggo/feed/backend/LeegoApplicationTests.java
@@ -0,0 +1,13 @@
+package leggo.feed.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeegoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
